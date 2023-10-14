420f7d30b9a66d378ff7533477f438abcdbcbc61
muniz94 <muniz91@hotmail.com>
2023-10-13 21:53:48 -0300

    Fiz a parte de validação do email

--- newsletter-sign-up-with-success-message-main/js/script.js
+++ newsletter-sign-up-with-success-message-main/js/script.js
@@ -1,26 +1,32 @@
-
+// Elements
 const input = document.getElementById('input_email');
 const form = document.getElementById('form');
+const emailErrorMessage = document.getElementById('emailErrorMessage');
+
+// Variables
+let ValidEmail;
+const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+
+const showErrorMessage = () => {
+  input.setAttribute('aria-invalid', true);
+  emailErrorMessage.classList.remove('error-msg_hide');
+};
+
+const hideErrorMessage = () => {
+  input.removeAttribute('aria-invalid');
+  emailErrorMessage.classList.add('error-msg_hide');
+}
+
+const validateEmail = () => {
+  const userEmail = input.value;
+  validRegex.test(userEmail) ? hideErrorMessage() : showErrorMessage();
+  ValidEmail = validRegex.test(userEmail);
+};
+
 
 form.addEventListener('submit', event => {
-  event.preventDefault();
-
-  const email = event.target.elements['input_email'];
-  validateEmail(email);
+   event.preventDefault();
+   
+   validateEmail();
 });
-
-function validateEmail(email){
-  let validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
-
-  console.log(email.value.match(validRegex))
-  if (email.value.match(validRegex))
-  {
-    alert("This is a valid email!");
-    email.value = "";
-  }
-  else {
-    alert("This isn't a valid email...");
-    email.value = "";
-    email.focus();
-  }
-}
