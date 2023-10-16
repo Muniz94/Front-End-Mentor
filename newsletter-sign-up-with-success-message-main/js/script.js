65b1a0efbd72df1ac0b7f3b9eb4a93b6709b6274
muniz94 <muniz91@hotmail.com>
2023-10-16 13:11:31 -0300

    Fiz o submit, troca de páginas, configurações CSS

--- newsletter-sign-up-with-success-message-main/js/script.js
+++ newsletter-sign-up-with-success-message-main/js/script.js
@@ -1,22 +1,28 @@
 // Elements
-const input = document.getElementById('input_email');
+const input = document.getElementById('form__input__email');
 const form = document.getElementById('form');
+const submitBtn = document.getElementById('subscribe');
 const emailErrorMessage = document.getElementById('emailErrorMessage');
+const clearBtn = document.getElementById('clearBtn');
+const mainSection = document.getElementById('section-main');
+const successSection = document.getElementById('section-success');
 
 // Variables
 let ValidEmail;
+let successPage = false;
 const validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
+// Functions
 
 const showErrorMessage = () => {
   input.setAttribute('aria-invalid', true);
-  emailErrorMessage.classList.remove('error-msg_hide');
+  emailErrorMessage.classList.remove('form__error-msg_hide');
 };
 
 const hideErrorMessage = () => {
   input.removeAttribute('aria-invalid');
-  emailErrorMessage.classList.add('error-msg_hide');
-}
+  emailErrorMessage.classList.add('form__error-msg_hide');
+};
 
 const validateEmail = () => {
   const userEmail = input.value;
@@ -24,9 +30,37 @@ const validateEmail = () => {
   ValidEmail = validRegex.test(userEmail);
 };
 
+const submitForm = () => {
+  const formData = new FormData(form);
+  emailValue.textContent = input.value;
+  changePage();
+  input.value = '';
+};
+
+const showSuccessSection = () => {
+  mainSection.classList.add('section_hide');
+  successSection.classList.remove('section_hide');
+  successPage = true;
+};
+
+const showMainSection = () => {
+  successSection.classList.add('section_hide');
+  mainSection.classList.remove('section_hide');
+  successPage = false;
+};
+
+const changePage = () => {
+  successPage ? showMainSection() : showSuccessSection();
+}
+
+// Events
 
 form.addEventListener('submit', event => {
    event.preventDefault();
    
    validateEmail();
+
+   if (ValidEmail) submitForm();
 });
+
+clearBtn.addEventListener('click', changePage);
